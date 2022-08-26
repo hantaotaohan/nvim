3cc0583bb0bdc85a0fb8b1b665869b9c14af4085
hantaotaohan <hantaotaohan@gmail.com>
2022-08-26 15:05:44 +0800

    add colorizer zen-mode

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -163,12 +163,12 @@ o.foldtext='NeatFoldText()'
 
 vim.cmd[[
     function! NeatFoldText() 
-        let line = ' ' . substitute(getline(v:foldstart), '^\s*"\?\s*\|\s*"\?\s*{{' . '{\d*\s*', '', 'g') . ' '
+        let line = "█  " . substitute(getline(v:foldstart), '^\s*"\?\s*\|\s*"\?\s*{{' . '{\d*\s*', '', 'g') . "  █" 
         let lines_count = v:foldend - v:foldstart + 1
-        let lines_count_text = '| ' . printf("%10s", lines_count . ' lines') . ' |'
+        let lines_count_text = ' ' . printf("%10s", lines_count . ' lines') . ' '
         let foldchar = matchstr(&fillchars, 'fold:\zs.')
-        let foldtextstart = strpart('+' . repeat(foldchar, v:foldlevel*2) . " ▉ FOLDED ▉ " . line , 0, (winwidth(0)*2)/3)
-        let foldtextend = lines_count_text . repeat(foldchar, 8)
+        let foldtextstart = strpart(" FOLDED █" . line , 0, (winwidth(0)*2)/3)
+        let foldtextend = "█" . lines_count_text . repeat(foldchar, 8) . "█"
         let foldtextlength = strlen(substitute(foldtextstart . foldtextend, '.', 'x', 'g')) + &foldcolumn
         return foldtextstart . repeat(foldchar, winwidth(0)-foldtextlength) . foldtextend
     endfunction
@@ -184,7 +184,8 @@ vim.cmd [[
 o.fillchars:append("stl: ")
 o.fillchars:append("stlnc: ")
 o.fillchars:append("vert:┃")
-o.fillchars:append("fold:─")
+o.fillchars:append("fold:█")
+-- o.fillchars:append("fold:─")
 o.fillchars:append("diff:⣿")
 o.fillchars:append("msgsep:‾")
 o.fillchars:append("eob: ")
--- lua/plugins/configs/colorizer.lua
+++ lua/plugins/configs/colorizer.lua
@@ -1,18 +1,16 @@
-local status_ok, colorizer = pcall(require, "colorizer")
+local present, colorizer = pcall(require, "colorizer")
 
-if not status_ok then
-  return
+if not present then
+    return
 end
 
-colorizer.setup({ "*" }, {
-  RGB = true, -- #RGB hex codes
-  RRGGBB = true, -- #RRGGBB hex codes
-  names = false, -- "Name" codes like Blue oe blue
-  RRGGBBAA = true, -- #RRGGBBAA hex codes
-  rgb_fn = true, -- CSS rgb() and rgba() functions
-  hsl_fn = true, -- CSS hsl() and hsla() functions
-  css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-  css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-  -- Available modes: foreground, background, virtualtext
-  mode = "background", -- Set the display mode.)
-})
+local options = {
+
+  '*'; -- Highlight all files, but customize some others.
+  css = { rgb_fn = true; }; -- Enable parsing rgb(...) functions in css.
+  html = { names = false; html = { mode = 'background' };}; -- Disable parsing "names" like Blue or Gray
+  lua = { names = false; html = { mode = 'background' };}; -- Disable parsing "names" like Blue or Gray
+
+}
+
+require "colorizer".setup(options)
