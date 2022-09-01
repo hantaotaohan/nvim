60fef254b3698e4be170430b3004d82522200239
TaoTao <hantaotaohan@gmail.com>
2022-09-01 13:02:01 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -23,7 +23,8 @@ if fn.empty(fn.glob(install_path)) > 0 then
     -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#23272E" })
     print "Cloning Packer ..."
-    fn.system { "git", "clone", "--depth", "1", "https://hub.fastgit.xyz/wbthomason/packer.nvim", install_path }
+    -- fn.system { "git", "clone", "--depth", "1", "https://hub.fastgit.xyz/wbthomason/packer.nvim", install_path }
+    fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
     vim.cmd "packadd packer.nvim"
 
     vim.cmd "set laststatus=0"
