42cc69d32b303a062bdeb22b43409c5384901a1c
hantaotaohan <hantaotaohan@gmail.com>
2022-09-15 16:14:46 +0800

    add comment-box.lua

---
+++ lua/plugins/configs/comment-box.lua
@@ -0,0 +1,32 @@
+local present, comment_box = pcall(require, "comment-box")
+
+if not present then
+    return
+end
+
+local options = {
+    doc_width = 80, -- width of the document
+    box_width = 70, -- width of the boxes
+    borders = { -- symbols used to draw a box
+        top = "─",
+        bottom = "─",
+        left = "│",
+        right = "│",
+        top_left = "╭",
+        top_right = "╮",
+        bottom_left = "╰",
+        bottom_rsight = "╯",
+    },
+    line_width = 70, -- width of the lines
+    line = { -- symbols used to draw a line
+        line = "─",
+        line_start = "─",
+        line_end = "─",
+    },
+    outer_blank_lines = false, -- insert a blank line above and below the box
+    inner_blank_lines = false, -- insert a blank line above and below the text
+    line_blank_line_above = false, -- insert a blank line above the line
+    line_blank_line_below = false, -- insert a blank line below the line
+}
+
+comment_box.setup(options)
